5cbbdf8c0d6725ae1598d1dd41a6fd29bad36d3a
tyetao <765068439@qq.com>
2019-01-08 20:47:24 +0800

    机构增加图片

--- src/dbscript/init/update.sql
+++ src/dbscript/init/update.sql
@@ -6,3 +6,5 @@ alter table `article` add `article_type` INT DEFAULT 0 COMMENT '0:资讯,1朋友
 alter table `article` add `category` INT DEFAULT 0 COMMENT '0:学生,1:老师:,2:家长,3:教育,4:动漫,5:音乐';
 
 alter table `article_comment` add `img_urls` varchar(1024) DEFAULT NULL COMMENT '文件url';
+
+alter table `institute` add `img_urls` varchar(1024) DEFAULT NULL COMMENT '文件url';
